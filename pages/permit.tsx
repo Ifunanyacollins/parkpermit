9ef8fc135017f4c95f5ac02ba712f1c80cb0fe39
collins <collinsogbuzuru@gmail.com>
2022-07-09 18:30:43 +0100

    add permit table

--- pages/permit.tsx
+++ pages/permit.tsx
@@ -1,12 +1,27 @@
 import type { NextPage } from "next";
+import Card from "../src/components/card";
 import Layout from "../src/components/Layout";
+import CountCard from "../src/components/StaticComponents/CountCard";
+import AllPermitTable from "../src/tables/AllPermitTable";
 
 const Permit: NextPage = () => {
   return (
     <Layout>
-      <h1>hello world</h1>
-      <h1>hello world</h1>
-      <h1>hello world</h1>
+      <div className="grid grid-cols-3 gap-3">
+        <CountCard title="Park Permit this year" count={1000000} />
+        <CountCard title="Park Permit this Month" count={40000} />
+        <CountCard title="Park Permit today" count={2000} />
+      </div>
+
+      <div className="my-10">
+        <Card>
+          <div className="mb-10 mt-5">
+            <p className="text-lg font-bold ">All Park Permit</p>
+          </div>
+
+          <AllPermitTable />
+        </Card>
+      </div>
     </Layout>
   );
 };
